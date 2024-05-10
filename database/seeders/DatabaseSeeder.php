e17829511e5b8847a990b677bc0cf6d2c1b4815d
diegorezm <dmourarezende@gmail.com>
2024-05-10 11:18:44 -0300

    fix: only one user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,12 +4,13 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        User::factory()->count(2)->create();
+        User::factory()->count(1)->create();
         $this->call([CourseSeeder::class]);
     }
 }
